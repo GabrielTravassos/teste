c7e67e2cafeed84a9cbcb5f8f5c18e54015654e7
Gabriel Soares Travassos <42143734+GabrielTravassos@users.noreply.github.com>
2023-02-23 17:31:54 -0300

    Add files via upload

---
+++ teste/exercicio.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Exercicio</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>exercicio</h1>
+        <h2>visualização do exercicios</h2>
+    </header>
+    <nav class="navegacao">
+        <a href="#" class="verde">Sem formatação</a>
+    </nav>
+    <main class="principal">
+        <div class="conteudo">
+            
+        </div>
+    </main>
+    <footer class="rodape">
+        EU © <?= date('Y'); ?>
+    </footer>
+    
+</body>
+</html>
---
+++ teste/index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Teste1234</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>teste php</h1>
+        <h2>indice de exercicios</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            <nav class="modulos">
+                <div class="modulo verde">
+                    <h3>modulo 1</h3>
+                    <ul>
+                        <li><a href="exercicio.php">exercicio A</a></li>
+                        <li><a href="#">exercicio B</a></li>
+                        <li><a href="#">exercicio C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo vermelho">
+                    <h3>modulo 2</h3>
+                    <ul>
+                        <li><a href="#">exercicio A</a></li>
+                        <li><a href="#">exercicio B</a></li>
+                        <li><a href="#">exercicio C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo azul">
+                    <h3>modulo 3</h3>
+                    <ul>
+                        <li><a href="#">exercicio A</a></li>
+                        <li><a href="#">exercicio B</a></li>
+                        <li><a href="#">exercicio C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo amarelo">
+                    <h3>modulo 4</h3>
+                    <ul>
+                        <li><a href="#">exercicio A</a></li>
+                        <li><a href="#">exercicio B</a></li>
+                        <li><a href="#">exercicio C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo rosa">
+                    <h3>modulo 5</h3>
+                    <ul>
+                        <li><a href="#">exercicio A</a></li>
+                        <li><a href="#">exercicio B</a></li>
+                        <li><a href="#">exercicio C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo roxo">
+                    <h3>modulo 6</h3>
+                    <ul>
+                        <li><a href="#">exercicio A</a></li>
+                        <li><a href="#">exercicio B</a></li>
+                        <li><a href="#">exercicio C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo ciano">
+                    <h3>modulo 7</h3>
+                    <ul>
+                        <li><a href="#">exercicio A</a></li>
+                        <li><a href="#">exercicio B</a></li>
+                        <li><a href="#">exercicio C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo laranja">
+                    <h3>modulo 8</h3>
+                    <ul>
+                        <li><a href="#">exercicio A</a></li>
+                        <li><a href="#">exercicio B</a></li>
+                        <li><a href="#">exercicio C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo preto">
+                    <h3>modulo 9</h3>
+                    <ul>
+                        <li><a href="#">exercicio A</a></li>
+                        <li><a href="#">exercicio B</a></li>
+                        <li><a href="#">exercicio C</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </main>
+    <footer class="rodape">
+        EU © <?= date('Y'); ?>
+    </footer>
+    
+</body>
+</html>
